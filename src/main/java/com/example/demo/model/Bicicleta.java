08be9701e3b881d95f62aabaa17918911142b184
EQUIPO <mateo.bernal2206@gmail.com>
2022-10-01 17:13:57 -0500

    retes

--- src/main/java/com/example/demo/model/Bicicleta.java
+++ src/main/java/com/example/demo/model/Bicicleta.java
@@ -8,71 +8,19 @@ import java.io.Serializable;
 @Entity
 @Table(name="Bike")
 public class Bicicleta implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="Id", unique=true ,nullable = false)
-    private int id;
-    @Column(name="brand", nullable = false)
-    private String marca;
-    @Column(name="year")
-    private Integer year;
-    @ManyToOne
-    @JoinColumn(name="category")
-    @JsonIgnoreProperties("bikes")
-    private Categoria category;
-    @Column(name="name", unique = true,nullable = false)
-    private String nombre;
-    @Column(name="description")
-    private String descrip;
-    @Column(name="messages")
-    String messages;
-    @ManyToOne
-    @JoinColumn(name="reservations")
-    @JsonIgnoreProperties("bikes")
-    private Reserva reservations;
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public String getMarca() {
-        return marca;
-    }
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-    public String getDescrip() {
-        return descrip;
-    }
-    public void setDescrip(String descrip) {
-        this.descrip = descrip;
-    }
-    public Categoria getCategory() {
-        return category;
-    }
-    public void setCategory(Categoria category) {
-        this.category = category;
-    }
-    public String getMessages() {
-        return messages;
-    }
-    public void setMessages(String messages) {
-        this.messages = messages;
-    }
-    public Reserva getReservations() {
-        return reservations;
-    }
-    public void setReservations(Reserva reservations) {
-        this.reservations = reservations;
-    }
+    @Entity
+    @Table(name = "bike")
+    public class Library implements Serializable {
     
-  
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Integer id;
+        private String name;
+        private String target;
+        private String description;
+        @ManyToOne
+        @JoinColumn(name = "categoryId")
+        @JsonIgnoreProperties("libs")
+        private Categoria category;
   
 }
--- src/main/java/com/example/demo/model/Categoria.java
+++ src/main/java/com/example/demo/model/Categoria.java
@@ -6,54 +6,92 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
-
-
-
 @Entity
 @Table(name = "category")
 public class Categoria implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer idCategory;
     private String name;
     private String description;
 
     @OneToMany(cascade = {CascadeType.PERSIST},mappedBy = "category")
-    @Column(name="bikes")
     @JsonIgnoreProperties("category")
-    private List<Bicicleta> bicicletas;
-    
-    public Integer getid() {
-        return id;
+    private List<Bicicleta> bikes;
+
+
+
+    @OneToMany(cascade = {CascadeType.PERSIST},mappedBy = "category")
+    @JsonIgnoreProperties("category")
+    private List<Reserva> reservation;
+
+
+
+    public Integer getIdCategory() {
+        return idCategory;
     }
 
-    public void setId(Integer id) {
-        this.id= id;
+
+
+    public void setIdCategory(Integer idCategory) {
+        this.idCategory = idCategory;
     }
+
+
 
     public String getName() {
         return name;
     }
 
+
+
     public void setName(String name) {
         this.name = name;
     }
+
+
 
     public String getDescription() {
         return description;
     }
 
+
+
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public List<Bicicleta> getbicicletas() {
-        return bicicletas;
+
+
+    public List<Bicicleta> getBikes() {
+        return bikes;
     }
 
-    public void setbicicletas (List<Bicicleta> bicicletas) {
-        this.bicicletas = bicicletas;
+
+
+    public void setBikes(List<Bicicleta> bikes) {
+        this.bikes = bikes;
     }
+
+
+
+    public List<Reserva> getReservation() {
+        return reservation;
+    }
+
+
+
+    public void setReservation(List<Reserva> reservation) {
+        this.reservation = reservation;
+    }
+
+
+    
 }
+
+
+
+
+
 
