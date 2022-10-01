8d15f7fe912224834db3f1bc701d8332062cfcfd
EQUIPO <mateo.bernal2206@gmail.com>
2022-10-01 17:36:48 -0500

    sgdalasd

--- src/main/java/com/example/demo/model/Reserva.java
+++ src/main/java/com/example/demo/model/Reserva.java
@@ -24,7 +24,7 @@ public class Reserva implements Serializable {
     private Date fechafin;
 
     @ManyToOne
-    @JoinColumn(name = "idCategory")
+    @JoinColumn(name = "category")
     @JsonIgnoreProperties("reservas")
     private Categoria category;
 
