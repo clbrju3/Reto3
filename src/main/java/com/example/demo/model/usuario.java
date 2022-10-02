815be4277d42f50ebbd658db3a1e2744713ff7cc
EQUIPO <mateo.bernal2206@gmail.com>
2022-10-02 14:47:11 -0500

    sdthadg

--- src/main/java/com/example/demo/model/usuario.java
+++ src/main/java/com/example/demo/model/usuario.java
@@ -4,7 +4,6 @@ import javax.persistence.*;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -23,4 +22,47 @@ public class usuario implements Serializable {
     @OneToMany(cascade = {CascadeType.PERSIST},mappedBy = "client")
         @JsonIgnoreProperties("client")
         private List<Reserva> reservations;
+    public Integer getIdClient() {
+        return idClient;
+    }
+    public void setIdClient(Integer idClient) {
+        this.idClient = idClient;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public List<Mensaje> getMessages() {
+        return messages;
+    }
+    public void setMessages(List<Mensaje> messages) {
+        this.messages = messages;
+    }
+    public List<Reserva> getReservations() {
+        return reservations;
+    }
+    public void setReservations(List<Reserva> reservations) {
+        this.reservations = reservations;
+    }
+        
 }
--- src/main/java/com/example/demo/repository/usureposit.java
+++ src/main/java/com/example/demo/repository/usureposit.java
@@ -1,10 +1,7 @@
 package com.example.demo.repository;
-import com.example.demo.model.Bicicleta;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.usuario;
-import javax.persistence.*;
-import java.util.ArrayList;
 
 @Repository
 public interface usureposit extends CrudRepository<usuario, Integer>{
