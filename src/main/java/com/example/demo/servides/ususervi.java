e7cfaaa80fabae887d1fa63d401e92ed885d8b82
EQUIPO <mateo.bernal2206@gmail.com>
2022-10-03 12:05:49 -0500

    gsdfae

--- src/main/java/com/example/demo/servides/ususervi.java
+++ src/main/java/com/example/demo/servides/ususervi.java
@@ -18,7 +18,7 @@ public class ususervi {
     usureposit usureposit;
 
     public ArrayList<usuario> resultado(){
-     jsontraer(1);
+     jsontraer();
         return (ArrayList<usuario>) usureposit.findAll();
     }
     public usuario guardar(usuario usua){
@@ -53,11 +53,16 @@ public class ususervi {
         }
     }
 
-    public void jsontraer(Integer id){
-        System.out.println((ArrayList<usuario>) usureposit.findAll());
-        Optional<usuario> lista=(Optional<usuario>) usureposit.findById(id);
-        if(lista.isPresent()){
-            System.out.println(lista.get().getAge());
+    public void jsontraer(){
+       // System.out.println((ArrayList<usuario>) usureposit.findAll());
+        List<usuario> lista=(List<usuario>) usureposit.findAll();
+        ArrayList g=null;
+        for(usuario x:lista){
+            g.add(x.getIdClient());
+            g.add(x.getName());
+            g.add(x.getAge());
+            g.add(x.getPassword());
+            g.add(x.getReservations());
         }
         ObjectMapper mapper=new ObjectMapper();
         /*try{
