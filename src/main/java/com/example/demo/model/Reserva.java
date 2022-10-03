b3278da94d5e038ab7e1316d810febd1ff2df520
EQUIPO <mateo.bernal2206@gmail.com>
2022-10-02 20:59:25 -0500

    gsdfae

--- src/main/java/com/example/demo/model/Reserva.java
+++ src/main/java/com/example/demo/model/Reserva.java
@@ -21,7 +21,7 @@ public class Reserva implements Serializable {
     private String status="created";
     @ManyToOne
     @JoinColumn(name = "id")
-        @JsonIgnoreProperties("reservations")
+        @JsonIgnoreProperties({"reservations","client"})
         private Bicicleta bike;
     @ManyToOne
     @JoinColumn(name="idClient")
