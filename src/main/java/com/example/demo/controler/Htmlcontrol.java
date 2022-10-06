176d0cdc33d3d6a822387d81d6fe822a6dab6994
EQUIPO <mateo.bernal2206@gmail.com>
2022-10-06 17:37:11 -0500

    gsdfae

--- src/main/java/com/example/demo/controler/Htmlcontrol.java
+++ src/main/java/com/example/demo/controler/Htmlcontrol.java
@@ -4,9 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Htmlcontrol {
-        @GetMapping("/")
-        public String index() {
-            return "Index";
+        @GetMapping("Vistas/mensage.html")
+        public String mensage() {
+            return "Vistas/html/mensage";
         }
+    @GetMapping("Vistas/bicicleta.html")
+    public String bici() {
+        return "Vistas/html/bicicleta";
     }
+    @GetMapping("Vistas/cliente.html")
+    public String cliente() {
+        return "/Vistas/hmtl/cliente";
+    }
+
+    }
+
 
