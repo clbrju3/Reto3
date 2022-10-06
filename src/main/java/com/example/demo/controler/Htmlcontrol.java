a593298f795902f5c979b41f1f7b211b9c2c7eac
EQUIPO <mateo.bernal2206@gmail.com>
2022-10-06 16:44:58 -0500

    gsdfae

--- src/main/java/com/example/demo/controler/Htmlcontrol.java
+++ src/main/java/com/example/demo/controler/Htmlcontrol.java
@@ -2,14 +2,11 @@ package com.example.demo.controler;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class Htmlcontrol {
-    @Controller
-    public class UserController {
-
         @GetMapping("/")
         public String index() {
             return "index";
         }
     }
-}
+
