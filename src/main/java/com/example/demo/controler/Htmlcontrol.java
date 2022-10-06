774dd89a360ca7535801bc26113469d341f5e54d
EQUIPO <mateo.bernal2206@gmail.com>
2022-10-06 17:40:53 -0500

    gsdfae

--- src/main/java/com/example/demo/controler/Htmlcontrol.java
+++ src/main/java/com/example/demo/controler/Htmlcontrol.java
@@ -2,8 +2,14 @@ package com.example.demo.controler;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class Htmlcontrol {
+    @GetMapping("/")
+    public String index(){
+        return "Index";
+    }
         @GetMapping("Vistas/mensage.html")
         public String mensage() {
             return "Vistas/html/mensage";
