eed09a81b804227af8dcd9cbd5911db60934a908
EQUIPO <mateo.bernal2206@gmail.com>
2022-10-06 18:56:46 -0500

    gsdfae

--- src/main/java/com/example/demo/controler/Htmlcontrol.java
+++ src/main/java/com/example/demo/controler/Htmlcontrol.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Htmlcontrol {
     @GetMapping("/")
     public String index(){
-        return "<h2>Index</h2>";
+        return "Index";
     }
         @GetMapping("/Vistas/mensage.html")
         public String mensage() {
